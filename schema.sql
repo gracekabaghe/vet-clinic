e74ceac48f6c0097d7f47f8612c644df9ce8c0cb
gracekabaghe <gracema2002@gmail.com>
2022-01-17 19:21:31 +0200

    created DB and animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,14 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+\c vet_clinic
+
+vet_clinic=# CREATE TABLE animals(
+vet_clinic(#   id            INT,
+vet_clinic(#   name    VARCHAR(250),
+vet_clinic(#   date_of_birth           date,
+vet_clinic(#   escape_attempts    INT,
+vet_clinic(#   neutered     boolean,
+vet_clinic(# weight INT,
+vet_clinic(#   PRIMARY KEY(id)
+vet_clinic(# );
