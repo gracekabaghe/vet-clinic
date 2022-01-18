457fdfbbc94519a757e177a6192a527d0bfc2e20
gracekabaghe <gracema2002@gmail.com>
2022-01-18 14:29:35 +0200

    inserted queries

--- schema.sql
+++ schema.sql
@@ -1,14 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 CREATE DATABASE vet_clinic;
 
-\c vet_clinic
-
-vet_clinic=# CREATE TABLE animals(
-vet_clinic(#   id            INT,
-vet_clinic(#   name    VARCHAR(250),
-vet_clinic(#   date_of_birth           date,
-vet_clinic(#   escape_attempts    INT,
-vet_clinic(#   neutered     boolean,
-vet_clinic(# weight INT,
-vet_clinic(#   PRIMARY KEY(id)
-vet_clinic(# );
+CREATE TABLE animals(
+  id                INT GENERATED ALWAYS AS IDENTITY,
+  name              VARCHAR(60),
+  date_of_birth     DATE,
+  escape_attempts   INT,
+  neutered          BOOLEAN,
+  weight_kg         DECIMAL,
+  PRIMARY KEY(id)
+);
