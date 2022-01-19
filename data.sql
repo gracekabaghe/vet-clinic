3960d29a9d8abca3cbf66368f726960b63bb4f90
gracekabaghe <gracema2002@gmail.com>
2022-01-19 13:20:21 +0200

    updated animals table

--- data.sql
+++ data.sql
@@ -19,3 +19,27 @@ VALUES ('Pikachu', '01-07-2021', '1', 'false', '15.08');
 INSERT INTO animals 
 (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 VALUES ('Devimon', '05-12-2017', '5', 'true', '11');
+
+INSERT INTO animals 
+(name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Charmander', '02-08-2020', '0', 'false', '11');
+
+INSERT INTO animals 
+(name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Plantmon', '11-15-2022', '2', 'true', '-5.7');
+
+INSERT INTO animals 
+(name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Squirtle', '04-02-1993', '3', 'false', '-12.13');
+
+INSERT INTO animals 
+(name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Angemon', '06-12-2005', '2', 'true', '-45');
+
+INSERT INTO animals 
+(name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Boarmon', '06-07-2005', '7', 'true', '20.4');
+
+INSERT INTO animals 
+(name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Blossom', '10-13-1998', '3', 'true', '17');
