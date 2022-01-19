41e84ee3bde8e2bfa38b5ec2f9b93b6e92765755
gracekabaghe <gracema2002@gmail.com>
2022-01-19 15:43:38 +0200

    ran queries and updates

--- queries.sql
+++ queries.sql
@@ -10,3 +10,22 @@ SELECT name,escape_attempts FROM animals WHERE weight_Kg > 10.5;
 SELECT * FROM animals WHERE neutered='true';
 SELECT * FROM animals WHERE NOT name='Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+SELECT COUNT(*) FROM animals;
+-- 9
+
+SELECT COUNT(*) FROM animals WHERE neutered = false;
+-- 3
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+-- 2
+
+SELECT AVG(weight_kg) FROM animals;
+-- 16.6488888888888889
+
+SELECT MAX(escape_attempts) FROM animals;
+-- 7
+
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth >= '01-01-1990' AND date_of_birth < '12-31-2000' GROUP BY species;
