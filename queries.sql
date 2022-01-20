7bf256c80ee83774b3f86774101dd3bf79fc2243
gracekabaghe <gracema2002@gmail.com>
2022-01-20 12:10:16 +0200

    used JOIN queries and updated tables

--- queries.sql
+++ queries.sql
@@ -50,3 +50,37 @@ SELECT MAX(escape_attempts) FROM animals;
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth >= '01-01-1990' AND date_of_birth < '12-31-2000' GROUP BY species;
+
+-- What animals belong to Melody Pond?
+
+SELECT animals.name FROM animals 
+JOIN owners ON animals.owner_id = owners.id 
+WHERE owners.full_name = 'Melody Pond';
+
+-- List of all animals that are pokemon (their type is Pokemon).
+
+SELECT animals.*, species.name AS species_name FROM animals 
+JOIN species ON animals.species_id = species.id WHERE species.name = 'Pokemon';
+
+-- List all owners and their animals, remember to include those that don't own any animal.
+
+SELECT animals.name AS animal_name, owners.fulL_name AS owner_name FROM owners 
+LEFT JOIN animals ON owners.id = animals.owner_id;
+
+-- How many animals are there per species?
+
+SELECT species.name, COUNT(*) FROM animals 
+JOIN species ON species.id = animals.species_id GROUP BY species.name;
+
+-- List all Digimon owned by Jennifer Orwell.
+
+SELECT animals.name AS animal_name, owners.full_name AS owner_name, species.name AS species_name FROM animals
+JOIN species ON species.id = animals.species_id JOIN owners ON owners.id = animals.owner_id WHERE species.name = 'Digimon' AND owners.full_name = 'Jennifer Orwell';
+-- List all animals owned by Dean Winchester that haven't tried to escape.
+
+SELECT * FROM animals
+JOIN owners ON animals.owner_id = owners.id 
+WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
+-- Who owns the most animals?
+SELECT owners.full_name, COUNT(animals.name) AS num_animals FROM owners
+LEFT JOIN animals ON owners.id = animals.owner_id GROUP BY owners.full_name ORDER BY num_animals DESC;
--- schema.sql
+++ schema.sql
@@ -24,3 +24,23 @@ CREATE TABLE species (
     id SERIAL PRIMARY KEY,
     name VARCHAR(60)
 );
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+-- add column species_id which is a foreign key referencing species table
+ALTER TABLE animals
+    ADD species_id INT, 
+    ADD CONSTRAINT species_id
+    FOREIGN KEY(species_id)
+    REFERENCES species(id);
+
+    -- Add column owner_id which is a foreign key referencing the owners table
+
+    ALTER TABLE animals 
+    ADD owners_id INT, 
+    ADD CONSTRAINT owners_id 
+    FOREIGN KEY(owners_id) 
+    REFERENCES owners(id);
+
+    
